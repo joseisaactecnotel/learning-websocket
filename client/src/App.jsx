f8bfe0dd76a95ece50c6f80f3b3fc806974d8461
joseisaactecnotel <joseisaactecnotel@gmail.com>
2025-09-09 16:53:08 -0300

    feat: criar página app.jsx com estrutura básica

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.css";
+const App = () => {
+  return (
+    <>
+      <section>
+        <h1>0</h1>
+        <button>+</button>
+        <button>-</button>
+      </section>
+    </>
+  );
+};
+
+export default App;
